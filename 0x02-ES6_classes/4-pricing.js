647c071838e985be52cb5b1789b55b83c16ea57b
Junior-SD <122011495+Junior-SD@users.noreply.github.com>
2024-04-09 02:35:21 +0100

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,49 +1,43 @@
-// 4-pricing.js
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
-  // Getter and Setter for 'amount'
   get amount() {
     return this._amount;
   }
 
-  set amount(newAmount) {
-    if (typeof newAmount === 'number') {
-      this._amount = newAmount;
+  set amount(val) {
+    if (typeof val === 'number') {
+      this._amount = val;
     } else {
-      throw new Error('Invalid data type. Amount must be a number.');
+      throw new TypeError('Amount must be a number');
     }
   }
 
-  // Getter and Setter for 'currency'
   get currency() {
     return this._currency;
   }
 
-  set currency(newCurrency) {
-    if (newCurrency instanceof Currency) {
-      this._currency = newCurrency;
+  set currency(val) {
+    if (val instanceof Currency) {
+      this._currency = val;
     } else {
-      throw new Error('Invalid data type. Currency must be an instance of Currency class.');
+      throw new TypeError('Currency must be an instance of Currency class');
     }
   }
 
-  // Method to display attributes in the specified format
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
-  // Static method to convert price based on conversion rate
   static convertPrice(amount, conversionRate) {
     if (typeof amount === 'number' && typeof conversionRate === 'number') {
       return amount * conversionRate;
-    } else {
-      throw new Error('Invalid data type. Amount and conversionRate must be numbers.');
     }
+    throw new TypeError('Amount and conversion rate must be numbers');
   }
 }
