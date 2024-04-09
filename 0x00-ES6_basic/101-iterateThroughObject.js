97f0891fc55c64c63c6f930a5e9b3389ecd4d989
Junior-SD <122011495+Junior-SD@users.noreply.github.com>
2024-04-09 02:22:39 +0100

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,44 +1,9 @@
-export default function createIteratorObject(report) {
-  /* eslint-disable */
-  const departments = Object.values(report);
-
-  let index = 0;
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  return {
-    next() {
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
-      }
-
-      const currentDepartment = departments[currentDepartmentIndex];
-
-      if (currentEmployeeIndex >= currentDepartment.length) {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-        return this.next();
-      }
-
-      const currentEmployee = currentDepartment[currentEmployeeIndex];
-      currentEmployeeIndex++;
-
-      return { value: currentEmployee, done: false };
-    }
-  };
-}
-
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-  let employee = reportWithIterator.next();
+  const employees = [];
 
-  while (!employee.done) {
-    result += employee.value + '|';
-    employee = reportWithIterator.next();
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
   }
 
-  // Remove the trailing '|'
-  result = result.slice(0, -1);
-
-  return result;
+  return employees.join(' | ');
 }
