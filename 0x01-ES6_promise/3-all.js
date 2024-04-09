dc4557b623a09cb8bfa1576a5c4007ec56ca13ab
Junior-SD <122011495+Junior-SD@users.noreply.github.com>
2024-04-09 02:27:33 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,10 @@
-import { uploadPhoto, createUser } from '../alx-frontend-javascript/utils.js'
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-    }).catch(() => console.log('Signup system offline'));
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((succ) => {
+      console.log(`${succ[0].body} ${succ[1].firstName} ${succ[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
